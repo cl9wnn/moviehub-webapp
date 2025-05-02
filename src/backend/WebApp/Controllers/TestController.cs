fde628feb977b1515ef8305b8d9d17925b5636e8
Egor <egor_udir@mail.ru>
2025-05-02 18:21:44 +0300

    added backend ASP.NET Web API

---
+++ src/backend/WebApp/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController: ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {  
+        var message = new
+        {
+            message = "Answer from ASP.NET",
+            timestamp = DateTime.Now
+        };
+
+        return Ok(message);
+    }
+}
