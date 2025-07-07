c0884b5351ce71a2fe865dd9994083c04c9d0564
Egor <egor_udir@mail.ru>
2025-07-07 13:30:16 +0300

    backend target uri is taken from docker-compose file now

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const backendTarget = process.env.API_TARGET || 'http://localhost:5000';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: backendTarget,
         changeOrigin: true,
         secure: false,
       },
     },
-  }
+  },
 })
